19080d741bd4006beb426ad77566b8ec0c8f4655
christopher-earl <cearl@umass.edu>
2023-06-28 09:59:36 -0400

    New server structure

--- SLURM/Server.py
+++ SLURM/Server.py
@@ -27,13 +27,20 @@ class Server:
                num_clients: int, iterations: int, call_type: str = 'init', **kwargs):
 
     # Load algorithm and client classes
-    algorithm = getattr(__import__(algorithm_path, fromlist=[algorithm_name]), algorithm_name)
-    client = getattr(__import__(client_path, fromlist=[client_name]), client_name)
+    self.algorithm = getattr(__import__(algorithm_path, fromlist=[algorithm_name]), algorithm_name)
+    self.client = getattr(__import__(client_path, fromlist=[client_name]), client_name)
+    self.run_name = run_name
+    self.algorithm_path = algorithm_path
+    self.algorithm_name = algorithm_name
+    self.client_path = client_path
+    self.client_name = client_name
+    self.num_clients = num_clients
+    self.iterations = iterations
 
     if call_type == "init":
 
       # Generate initial 10 genes
-      alg = algorithm(run_name=run_name, **kwargs)
+      alg = self.algorithm(run_name=run_name, **kwargs)
       init_genes = []
       for i in range(num_clients):
         init_genes.append(alg.fetch_gene())
@@ -48,65 +55,70 @@ class Server:
         p = subprocess.Popen(bash_args + [f"--gene_name={g_name}"] + [f"--client_id={i}"])
 
     elif call_type == "run_client":
-
-      # Run gene
-      gene_name = kwargs['gene_name']
-      clnt = client(run_name, gene_name)
-      fitness = clnt.run()
-
-      # Return fitness (by writing to files)
-      gene_data = clnt.gene_data
-      gene_data['fitness'] = fitness
-      gene_data['status'] = 'tested'
-      pool_lock_path = file_path(run_name, POOL_LOCK_NAME)
-      with portalocker.Lock(pool_lock_path, timeout=100) as _:
-        write_gene(gene_data, gene_name, run_name)
-
-        # Write gene to logs
-        timestamp = time.strftime('%H:%M:%S', time.localtime())
-        log_data = {'timestamp': timestamp, 'gene_name': gene_name, 'gene_data': gene_data}
-        self.write_logs(run_name, kwargs['client_id'], log_data)  # Separate logs by client_id
-
-      # Callback server
-      bash_args = make_bash_args(run_name=run_name, algorithm_path=algorithm_path, algorithm_name=algorithm_name,
-                                 client_path=client_path, client_name=client_name, num_clients=num_clients,
-                                 iterations=iterations, call_type="server_callback", **kwargs)
-      p = subprocess.Popen(bash_args)
+      self.run_client(**kwargs)
 
     elif call_type == "server_callback":
-      count = int(kwargs.pop('count'))
-      iterations = int(iterations)    # Comes back as str from bash
-      count += 1
-      if count >= iterations:
-        sys.exit()
-
-      # Lock pool during gene creation
-      pool_lock_path = file_path(run_name, POOL_LOCK_NAME)
-      while True:
-        with portalocker.Lock(pool_lock_path, timeout=100) as _:
-
-          # Init alg (loads gene pool)
-          alg = algorithm(run_name=run_name, **kwargs)
-
-          # Fetch next gene for testing
-          gene_name, success = alg.fetch_gene()
-
-        # Break if fetch was success, otherwise loops
-        if success:
-          break
-        else:
-          time.sleep(1)
-
-      # Remove old gene_name from args, and send new gene to client
-      kwargs.pop('gene_name')
-      bash_args = make_bash_args(run_name=run_name, algorithm_path=algorithm_path, algorithm_name=algorithm_name,
-                                 client_path=client_path, client_name=client_name, num_clients=num_clients,
-                                 iterations=iterations, call_type="run_client", gene_name=gene_name,
-                                 count=count, **kwargs)
-      p = subprocess.Popen(bash_args)
+      self.server_callback(**kwargs)
 
     else:
       raise Exception(f"error, improper call_type: {call_type}")
+
+  def run_client(self, **kwargs):
+    # Run gene
+    gene_name = kwargs['gene_name']
+    clnt = self.client(self.run_name, gene_name)
+    fitness = clnt.run()
+
+    # Return fitness (by writing to files)
+    gene_data = clnt.gene_data
+    gene_data['fitness'] = fitness
+    gene_data['status'] = 'tested'
+    pool_lock_path = file_path(self.run_name, POOL_LOCK_NAME)
+    with portalocker.Lock(pool_lock_path, timeout=100) as _:
+      write_gene(gene_data, gene_name, self.run_name)
+
+      # Write gene to logs
+      timestamp = time.strftime('%H:%M:%S', time.localtime())
+      log_data = {'timestamp': timestamp, 'gene_name': gene_name, 'gene_data': gene_data}
+      self.write_logs(self.run_name, kwargs['client_id'], log_data)  # Separate logs by client_id
+
+    # Callback server
+    bash_args = make_bash_args(run_name=self.run_name, algorithm_path=self.algorithm_path, algorithm_name=self.algorithm_name,
+                               client_path=self.client_path, client_name=self.client_name, num_clients=self.num_clients,
+                               iterations=self.iterations, call_type="server_callback", **kwargs)
+    p = subprocess.Popen(bash_args)
+
+  def server_callback(self, **kwargs):
+    count = int(kwargs.pop('count'))
+    iterations = int(self.iterations)  # Comes back as str from bash
+    count += 1
+    if count >= iterations:
+      sys.exit()
+
+    # Lock pool during gene creation
+    pool_lock_path = file_path(self.run_name, POOL_LOCK_NAME)
+    while True:
+      with portalocker.Lock(pool_lock_path, timeout=100) as _:
+
+        # Init alg (loads gene pool)
+        alg = self.algorithm(run_name=self.run_name, **kwargs)
+
+        # Fetch next gene for testing
+        gene_name, success = alg.fetch_gene()
+
+      # Break if fetch was success, otherwise loops
+      if success:
+        break
+      else:
+        time.sleep(1)
+
+    # Remove old gene_name from args, and send new gene to client
+    kwargs.pop('gene_name')
+    bash_args = make_bash_args(run_name=self.run_name, algorithm_path=self.algorithm_path, algorithm_name=self.algorithm_name,
+                               client_path=self.client_path, client_name=self.client_name, num_clients=self.num_clients,
+                               iterations=iterations, call_type="run_client", gene_name=gene_name,
+                               count=count, **kwargs)
+    p = subprocess.Popen(bash_args)
 
   def write_logs(self, run_name: str, log_name: str, log_data: dict):
 
