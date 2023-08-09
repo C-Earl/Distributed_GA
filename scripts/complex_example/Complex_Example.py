29070bdd698cc8be663b4fedbce4d80fa335f745
christopher-earl <cearl@umass.edu>
2023-08-09 14:00:06 -0400

    More fixes

--- scripts/complex_example/Complex_Example.py
+++ scripts/complex_example/Complex_Example.py
@@ -1,13 +1,11 @@
 from DGA.Algorithm import Genetic_Algorithm_Base, Genetic_Algorithm
 from DGA.Client import Client
 from DGA.Server import Server
-import numpy as np
 import torchvision
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.transforms import ToTensor
-from math import prod
 
 DEVICE = torch.device('cpu')    # CPU will be faster than GPU for this example
 
@@ -17,22 +15,64 @@ class ArtificialNeuralNet(nn.Module):
     super().__init__()
     self.fc1 = nn.Linear(28 * 28, 120)
     self.fc2 = nn.Linear(120, 10)
-    # self.fc3 = nn.Linear(60, 10)
 
     # Set weights manually based on gene
     weights = [gene['l1'], gene['l2']]# , gene['l3']]
     with torch.no_grad():
       self.fc1.weight = torch.nn.parameter.Parameter(torch.tensor(weights[0], dtype=torch.float32).T.to(DEVICE))
       self.fc2.weight = torch.nn.parameter.Parameter(torch.tensor(weights[1], dtype=torch.float32).T.to(DEVICE))
-      # self.fc3.weight = torch.nn.parameter.Parameter(torch.tensor(weights[2], dtype=torch.float32).T.to(DEVICE))
 
   def forward(self, x):
     x = x.to(DEVICE)
     x = torch.flatten(x, 1)  # flatten all dimensions except batch
     x = F.relu(self.fc1(x))
     x = F.relu(self.fc2(x))
-    # x = self.fc3(x)
     return x
+
+
+#
+# Use the Client class to load your own models into DGA. the run() function
+# will be called to test your model. The run() function must return a float
+# value representing fitness.
+# # # # # # # # # # # # # # # # # # # # # #
+class Complex_GA_Client(Client):  # <--- Remember to inherit Client class
+
+  # Load data before running. All file access in load_data is read/write safe, ie. no other subprocess will
+  # access the same file at the same time. This is to prevent any OS read/write errors.
+  def load_data(self, **kwargs):
+    # trainset = torchvision.datasets.MNIST(root='./data', train=True, transform=ToTensor(),
+    #                                       download=True)
+    # self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=10,
+    #                                           shuffle=True, num_workers=2)
+    testset = torchvision.datasets.MNIST(root='./data', train=False, transform=ToTensor(),
+                                         download=True)
+    self.testloader = torch.utils.data.DataLoader(testset, batch_size=10,
+                                             shuffle=False, num_workers=2)
+
+  # Run your model here. The run() function must return a float value representing fitness.
+  # Here, we create an ANN which will try to classify handwritten digits from the MNIST dataset.
+  def run(self, gene, **kwargs) -> float:
+
+    # Create optimizer and ANN
+    ANN = ArtificialNeuralNet(gene)
+    ANN.to(DEVICE)
+
+    # Test ANN
+    correct = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+      for data in self.testloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = ANN(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    # Return fitness (ie. accuracy)
+    return correct / total
 
 
 if __name__ == '__main__':
--- scripts/simple_example/Simple_Example.py
+++ scripts/simple_example/Simple_Example.py
@@ -1,28 +1,39 @@
-from DGA.Algorithm import Genetic_Algorithm_Base, Genetic_Algorithm
+from DGA.Algorithm import Genetic_Algorithm
 from DGA.Client import Client
 from DGA.Server import Server
-import numpy as np
 
-
-# # # # # # # # # # # # # # # # # # # # # #
-# Use the Client class to load your own models into DGA. the run() function
+#
+# Use the Client class to load your models into DGA. run() function
 # will be called to test your model. The run() function must return a float
 # value representing fitness.
 # # # # # # # # # # # # # # # # # # # # # #
-class Simple_Client(Client):  # <--- Remember to inherit Client class
+class Simple_Client(Client):                  # <--- Remember to inherit Client class!
   def run(self, gene, **kwargs) -> float:
-    fitness = sum([-(i ** 2) for i in gene])
-    return fitness
+    fitness = sum([-(i ** 2) for i in gene])  # Fitness is (negative) sum of squares
+    return fitness                            # Optimization is to maximize fitness
+                                              # Perfectly optimized gene is all zeros
 
-
+#
+# The Server class is used to run the genetic algorithm.
+# Arguments:
+#   run_name: Name of run (run files saved in a folder with this name)
+#   algorithm: Algorithm for optimizing your model
+#   client: Client class with your model
+#   num_parallel_processes: Number of subprocesses to run in parallel
+#   iterations: Number of genes each subprocess will test
+#   **kwargs: Any additional parameters, including args specific to your algorithm.
+#             Args are passed automatically to the algorithm and client classes, and
+#             can be accessed with self.name_of_kwarg.
+# # # # # # # # # # # # # # # # # # # # # #
 if __name__ == '__main__':
   import os
 
   alg_path = os.path.abspath(__file__)
   client_path = os.path.abspath(__file__)
 
-  Server(run_name="simple_example",
-         algorithm=Genetic_Algorithm,
-         client=Simple_Client,
-         num_parallel_processes=5, gene_shape=(10,), num_genes=10, mutation_rate=0.25,
-         iterations=20)
+  Server(run_name="simple_example",     # Name of run (run files saved in a folder with this name)
+         algorithm=Genetic_Algorithm,   # Algorithm for optimizing your model
+         client=Simple_Client,          # Client class with your model
+         num_parallel_processes=5,      # Number of subprocesses to run in parallel
+         iterations=20,                 # Number of genes each subprocess will test
+         gene_shape=(10,), num_genes=10, mutation_rate=0.25)    # Algorithm parameters
