075cd4f579fe75c95b8377c48bc2405483b9afa2
christopher-earl <cearl@umass.edu>
2023-12-20 13:02:14 -0500

    small update

--- scripts/Local_example/Local_example.py
+++ scripts/Local_example/Local_example.py
@@ -29,7 +29,7 @@ class Vector_Estimation_Model(Model):  # <--- Remember to inherit Model class!
       else:
         for i in range(num_vectors):
           location = np.random.uniform(low=-vector_distribution, high=+vector_distribution)
-          self.target_vectors.append(np.random.normal(loc=location, scale=1, size=vector_size))
+          self.target_vectors.append(np.random.normal(loc=location, scale=3, size=vector_size))
 
   def run(self, gene, **kwargs) -> float:
     smallest_diff = np.inf
@@ -46,6 +46,17 @@ class Vector_Estimation_Model(Model):  # <--- Remember to inherit Model class!
 
 class Custom_Plateau_Algorithm(Plateau_Genetic_Algorithm):
 
+  def __init__(self, penalty_modulator, max_penalty, **kwargs):
+    super().__init__(**kwargs)
+    self.penalty_modulator = penalty_modulator
+    self.max_penalty = max_penalty
+    self.mutation_scale = 5
+    self.mutation_scale_decay = 0.9999
+
+  def fetch_gene(self, **kwargs):
+    self.mutation_scale *= self.mutation_scale_decay
+    return super().fetch_gene(**kwargs)
+
   def initial_gene(self, **kwargs):
     return np.random.normal(loc=0, scale=10, size=self.gene_shape)
 
@@ -53,43 +64,41 @@ class Custom_Plateau_Algorithm(Plateau_Genetic_Algorithm):
   def mutate(self, gene, **kwargs):
     if np.random.rand() < self.mutation_rate:
       mutation_start = np.random.randint(0, np.prod(self.gene_shape))
-      mutation_end = np.random.randint(mutation_start+1, mutation_start+2+(self.mutation_rate*5))
+      mutation_end = np.random.randint(mutation_start+1, mutation_start+2+10)
       if mutation_end > np.prod(self.gene_shape):
         mutation_end = np.prod(self.gene_shape)
       gene = gene.flatten()
-      gene[mutation_start:mutation_end] += np.random.normal(loc=0, scale=self.mutation_rate*5, size=mutation_end-mutation_start)
+      gene[mutation_start:mutation_end] += np.random.normal(loc=0, scale=self.mutation_scale, size=mutation_end-mutation_start)
     return gene.reshape(self.gene_shape)
 
   def founder_proximity_penalty(self, gene):
-    return super().founder_proximity_penalty(gene) / 7
+    penalty = min(super().founder_proximity_penalty(gene), self.max_penalty) # Cap penalty to radius
+    norm_penalty = penalty / self.max_penalty    # Normalize penalty to 0-1
+    return norm_penalty * self.penalty_modulator    # Apply modulator penalty
 
+  def start_new_epoch(self, **kwargs):
+    super().start_new_epoch(**kwargs)
+    self.mutation_scale = 5
 
-#
-# The Server class is used to run the genetic algorithm.
-# Arguments:
-#   run_name: Name of run (run files saved in a folder with this name)
-#   algorithm: Algorithm for optimizing your model
-#   model: Model class with your model
-#   num_parallel_processes: Number of subprocesses to run in parallel
-#   iterations: Number of genes each subprocess will test
-#   **kwargs: Any additional parameters, including args specific to your algorithm.
-#             Args are passed automatically to the algorithm and model classes, and
-#             can be accessed with self.name_of_kwarg.
-# # # # # # # # # # # # # # # # # # # # # #
 if __name__ == '__main__':
   import matplotlib.pyplot as plt
 
-  num_epochs = 10
-  alg = Custom_Plateau_Algorithm(gene_shape=(vector_size, vector_size),
+  num_epochs = 3
+  vector_size = (10, 10)
+  vector_distribution = 3
+  alg = Custom_Plateau_Algorithm(gene_shape=vector_size,
                                  num_genes=10,
                                  mutation_rate=1,
-                                 mutation_decay=.99995,
-                                 iterations_per_epoch=15_000,
+                                 mutation_decay=.9999,
+                                 iterations_per_epoch=25_000,
                                  epochs=num_epochs,
-                                 plateau_sensitivity=1e-5,
+                                 plateau_sensitivity=5e-4,
                                  plateau_sample_size=2000,
-                                 warmup=2000)
-  mod = Vector_Estimation_Model()
+                                 warmup=2000,
+                                 penalty_modulator=400,
+                                 max_penalty=1000,
+                                 )
+  mod = Vector_Estimation_Model(num_vectors=3, vector_size=vector_size, vector_distribution=vector_distribution)
   sync = Synchronized(run_name="Local_Complex_GA_Example",  # Name of run (run files saved in a folder with this name)
          algorithm=alg,  # Algorithm for optimizing your model
          model=mod,)  # Model class with your model
@@ -99,7 +108,7 @@ if __name__ == '__main__':
   for _, gene_data in alg.founders_pool.items():
     # Find which target vector the gene is closest to
     smallest_diff = np.inf
-    for i, targ in enumerate([mod.target_vector_1, mod.target_vector_2, mod.target_vector_3]):
+    for i, targ in enumerate(mod.target_vectors):
       diff = np.linalg.norm(gene_data['gene'].flatten() - targ.flatten())
       if diff < smallest_diff:
         smallest_diff = diff
@@ -108,9 +117,9 @@ if __name__ == '__main__':
 
     # Plot gene
     fig, ax = plt.subplots(1, 2)
-    im1 = ax[0].imshow(gene_data['gene'], vmax=1, vmin=-1, cmap='bwr')
+    im1 = ax[0].imshow(gene_data['gene'], vmax=+vector_distribution, vmin=-vector_distribution, cmap='bwr')
     ax[0].set_title("Gene")
-    ax[1].imshow(closest_targ, vmax=1, vmin=-1, cmap='bwr')
+    ax[1].imshow(closest_targ, vmax=+vector_distribution, vmin=-vector_distribution, cmap='bwr')
     ax[1].set_title(f"Closest Target Vector {closest_targ_id}")
     plt.colorbar(im1, ax=fig.get_axes(), orientation='horizontal')
 
