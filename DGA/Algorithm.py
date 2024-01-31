SLURM changes Fix probability calculation and sleep time in Server.py

DGA/Algorithm.py
@@ -225,8 +225,16 @@ class Genetic_Algorithm(Genetic_Algorithm_Base):
     fitness_scores = [params.fitness for params in params_list]
     normed_fitness = self.pos_normalize(fitness_scores)  # Shift fitness's to [0, +inf)
     probabilities = normed_fitness / np.sum(normed_fitness)  # Normalize to [0, 1]
+
+    max_ind = np.argmax(probabilities)
+    for i, p in enumerate(probabilities):
+      if p == 0:
+        probabilities[i] += 1e-5
+        probabilities[max_ind] -= 1e-5
+
     parent_inds = np.random.choice(np.arange(len(probabilities)), replace=False, p=probabilities,
-                                   size=self.num_parents)
+                                    size=self.num_parents)
+                                   
     return [params_list[i] for i in parent_inds]
 
   # Crossover parents to create offspring (according to user provided Genome)

DGA/Server.py
@@ -167,7 +167,7 @@ class Server:
     # Lock pool files during params creation to prevent race condition
     pool_lock_path = file_path(self.run_name, POOL_LOCK_NAME)
     while True:
-      with portalocker.Lock(pool_lock_path, timeout=100) as _:
+      with portalocker.Lock(pool_lock_path, timeout=500) as _:
 
         # Setup algorithm by loading from file
         # - updates pool and history based on files which other agents may have changed
@@ -201,7 +201,7 @@ class Server:
 
           break
         else:
-          time.sleep(1)
+          time.sleep(np.random.rand())
 
     # Remove old params_name from args, and send new params to model
     kwargs.pop('params_name')